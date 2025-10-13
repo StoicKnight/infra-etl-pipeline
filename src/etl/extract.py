955cfcaf59b5f7fa64c07862593e2cded9c7bd8e
Stoic Knight <alexis.skouris@pm.me>
2025-10-13 17:23:44 +0300

    feat(netbox): functions to extract netbox data

--- src/etl/extract.py
+++ src/etl/extract.py
@@ -1,9 +1,14 @@
 import json
+import logging
 from pathlib import Path
 from typing import Any, Dict, List
 
+from src.services.netbox.client import NetBoxAPIClient
 
-def load_json_file(file_path: Path) -> Dict[str, Any]:
+log = logging.getLogger(__name__)
+
+
+def load_json_data(file_path: Path) -> Dict[str, Any]:
     try:
         with open(file_path, "r", encoding="utf-8") as file:
             return json.load(file)
@@ -12,5 +17,47 @@ def load_json_file(file_path: Path) -> Dict[str, Any]:
         return {}
 
 
-def extract_data_from_files(file_paths: List[Path]) -> List[Dict[str, Any]]:
-    return list(map(load_json_file, file_paths))
+def extract_json_data(file_paths: List[Path]) -> List[Dict[str, Any]]:
+    return list(map(load_json_data, file_paths))
+
+
+async def list_netbox_ips(
+    client: NetBoxAPIClient,
+):
+    ips_list = []
+    try:
+        log.info("Fetching IP addresses from NetBox...")
+        async for ip in client.ips.list():
+            ips_list.append(ip)
+        log.info(f"Fetched {len(ips_list)} IPs.")
+        return ips_list
+    except Exception as e:
+        log.error(f"An unexpected error occurred: {e}")
+
+
+async def list_netbox_vms(
+    client: NetBoxAPIClient,
+):
+    vms_list = []
+    try:
+        log.info("Fetching Virtual Machines from NetBox...")
+        async for ip in client.vms.list():
+            vms_list.append(ip)
+        log.info(f"Fetched {len(vms_list)} VMs.")
+        return vms_list
+    except Exception as e:
+        log.error(f"An unexpected error occurred: {e}")
+
+
+async def list_netbox_devices(
+    client: NetBoxAPIClient,
+):
+    devices_list = []
+    try:
+        log.info("Fetching Devices from NetBox...")
+        async for ip in client.devices.list():
+            devices_list.append(ip)
+        log.info(f"Fetched {len(devices_list)} Devices.")
+        return devices_list
+    except Exception as e:
+        log.error(f"An unexpected error occurred: {e}")
--- src/services/netbox/client.py
+++ src/services/netbox/client.py
@@ -1,14 +1,10 @@
-import asyncio
 import logging
 
 import httpx
 
-from src.config import settings
-from src.logging import setup_logging
 from src.services.netbox.endpoints.dcim import DevicesEndpoints
 from src.services.netbox.endpoints.ipam import IPAddressesEndpoints
 from src.services.netbox.endpoints.virtualization import VMEndpoints
-from src.services.netbox.exceptions import NetBoxAPIError
 
 log = logging.getLogger(__name__)
 
@@ -19,89 +15,15 @@ class NetBoxAPIClient:
             "Authorization": f"Token {token}",
             "Accept": "application/json",
         }
-        self._client = httpx.AsyncClient(
+        self.__client = httpx.AsyncClient(
             base_url=base_url,
             headers=headers,
             timeout=20.0,
             verify=verify_ssl,
         )
-        self.devices = DevicesEndpoints(self._client)
-        self.vms = VMEndpoints(self._client)
-        self.ips = IPAddressesEndpoints(self._client)
+        self.devices = DevicesEndpoints(self.__client)
+        self.vms = VMEndpoints(self.__client)
+        self.ips = IPAddressesEndpoints(self.__client)
 
     async def close(self):
-        await self._client.aclose()
-
-
-async def main():
-    """
-    Example to test functionality of NetBoxAPIClient.
-    """
-    setup_logging()
-    NETBOX_BASE_URL = str(settings.netbox.base_url)
-    NETBOX_API_TOKEN = settings.netbox.api_token
-    VERIFY_SSL = False
-    DEVICE_ID = 1547
-    VM_ID = 836
-    IP_ADDRESS_ID = 13050
-
-    log.info("Initializing NetBox API client...")
-    client = NetBoxAPIClient(
-        base_url=NETBOX_BASE_URL, token=NETBOX_API_TOKEN, verify_ssl=VERIFY_SSL
-    )
-    device_count = 0
-    vm_count = 0
-    ip_count = 0
-
-    try:
-        log.info("Fetching devices from NetBox...")
-        async for device in client.devices.list():
-            device_count += 1
-            print(200 * "=")
-            log.info(f"Device #{device_count}: {device.name}")
-            print(
-                f"IPs: [{device.primary_ip.address if device.primary_ip else ""},{device.oob_ip.address if device.oob_ip else ""}]"
-            )
-            print(200 * "=")
-        log.info(f"\nFinished processing. Total devices found: {device_count}")
-
-        log.info(f"Fetching device ID: {DEVICE_ID}")
-        single_device = await client.devices.get(DEVICE_ID)
-        log.info(f"Successfully fetched device: {single_device.name}")
-
-        log.info("Fetching VMs from NetBox...")
-        async for vm in client.vms.list():
-            vm_count += 1
-            print(200 * "=")
-            log.info(f"VM #{vm_count}: {vm.name}")
-            print(f"IPs: [{vm.primary_ip.address if vm.primary_ip else ""}]")
-            print(200 * "=")
-        log.info(f"\nFinished processing. Total VMs found: {vm_count}")
-
-        log.info(f"Fetching VM ID: {VM_ID}")
-        single_vm = await client.vms.get(VM_ID)
-        log.info(f"Successfully fetched VM: {single_vm.name}")
-
-        log.info("Fetching IP addresses from NetBox...")
-        async for ip in client.ips.list():
-            ip_count += 1
-            print(200 * "=")
-            log.info(f"IP #{vm_count}: {ip.display}")
-            print(f"Address: [{ip.address}]")
-            print(200 * "=")
-        log.info(f"\nFinished processing. Total IPs found: {ip_count}")
-
-        log.info(f"Fetching IP address ID: {IP_ADDRESS_ID}")
-        single_ip = await client.ips.get(IP_ADDRESS_ID)
-        log.info(f"Successfully fetched IP: {single_ip.address}")
-    except NetBoxAPIError as e:
-        log.error(f"API error: {e}")
-    except Exception as e:
-        log.error(f"An unexpected error occurred: {e}")
-    finally:
-        log.info("Closing API client.")
-        await client.close()
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+        await self.__client.aclose()
